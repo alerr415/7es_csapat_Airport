6fd7a7da2bf178387bb5feda9d3e0dd31f0fb07e
alerr415 <alerr415@hotmail.com>
2020-11-20 11:59:13 +0100

    test4

--- repuloter.cpp
+++ repuloter.cpp
@@ -1,5 +1,15 @@
 #include "repuloter.h"
 
+std::list<Jarat> Repuloter::getIndulo() const
+{
+    return indulo;
+}
+
+std::list<Jarat> Repuloter::getErkezo() const
+{
+    return erkezo;
+}
+
 Repuloter::Repuloter()
 {
 
