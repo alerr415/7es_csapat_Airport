1df5e3476d6f63fe6989bc38383170fbeeacf386
Toth-Balint1225 <74766154+Toth-Balint1225@users.noreply.github.com>
2020-11-20 12:59:48 +0100

    test11
    final test

--- adminisztrator.cpp
+++ adminisztrator.cpp
@@ -1,5 +1,8 @@
 #include "adminisztrator.h"
 //test10
+// még mindig nem tudom, mi a gond a QT-vel
+// a Git-Hub appban meg elfelejtettem kitölteni a commit descriptiont, az volt a baj
+// részemről üzemkész TB
 Adminisztrator::Adminisztrator()
 {
   
