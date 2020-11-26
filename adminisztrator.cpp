57001efd3bc2c161f5d4c3b12a3ab590160642c6
Toth-Balint1225 <74766154+Toth-Balint1225@users.noreply.github.com>
2020-11-26 15:18:50 +0100

    lehetséges vezérlés a main() függvényben
    egy kis multithreading, így percenként frissítti a képernyőt és [ENTER]-rel lehet megszakítani. Ez csak egy lehetséges megoldás (én se vagyok teljesen biztos benne), de szerintem egyszerűbben nem tudjuk megoldani, hogy frissüljön is a képernyő és reagáljon is a billentyűnyomásokra

--- adminisztrator.cpp
+++ adminisztrator.cpp
@@ -1,4 +1,8 @@
 #include "adminisztrator.h"
+#include <iostream>
+#include <fstream>
+
+
 Adminisztrator::Adminisztrator(std::string _felhasznalonev, std::string _jelszo):
     Operator(_felhasznalonev,_jelszo)
 {
@@ -16,5 +20,32 @@ void Adminisztrator::modosit()
 
 void Adminisztrator::teljesBevitel() const
 {
+    // bejelntekés után vagyunk, azt a main végzi
+    // beviteli adatok megjelenítése
+    std::cout << "Adja meg a beviteli módot:\n  1 : manuális\n  2 : fájlból olvasás" << std::endl;
+
+/*
+// teljes adatbevitel fileolvasás része
+    Repuloter& r = Repuloter::getInstance();
+    ifstream input;
+    input.open("tester.csv",ios_base::in);
+    if (!input.is_open())
+        return -1;
+    string line;
+    while (getline(input,line))
+    {
+        // azonosito 0; honnan 1; hova 2; indulas 3; erkezes 4; keses 5
+        vector<string> lineComponents = String::split<vector<string>>(line,';');
+        vector<string> dateComponents = String::split<vector<string>>(lineComponents[3],'-');
+        Datum indul(String::strToNum<unsigned>(dateComponents[0]),String::strToNum<unsigned>(dateComponents[1]),String::strToNum<unsigned>(dateComponents[2]),
+                String::strToNum<unsigned>(dateComponents[3]),String::strToNum<unsigned>(dateComponents[4]));
+        dateComponents.clear();
+        dateComponents = String::split<vector<string>>(lineComponents[4],'-');
+        Datum erkezik(String::strToNum<unsigned>(dateComponents[0]),String::strToNum<unsigned>(dateComponents[1]),String::strToNum<unsigned>(dateComponents[2]),
+                String::strToNum<unsigned>(dateComponents[3]),String::strToNum<unsigned>(dateComponents[4]));
+        Jarat toAdd(lineComponents[0],lineComponents[1],lineComponents[2],indul,erkezik,String::strToNum<unsigned>(lineComponents[5]));
+        r.addJarat(toAdd);
+    }
+*/
 
 }
--- main.cpp
+++ main.cpp
@@ -3,35 +3,109 @@
 #include <fstream>
 #include <vector>
 #include "stringhandler.h"
+#include <chrono>
+#include <cstdlib>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+
 
 using namespace std;
 
+
+void printTime(const std::chrono::system_clock::time_point& tp)
+{
+    time_t tp_t = std::chrono::system_clock::to_time_t(tp);
+    std::cout << ctime(&tp_t) << std::endl;
+}
+
+/*
+   Működés:
+   alapállapot: 1 percenként frissítve kiírja a képernyőre az időt (minta)
+   [ENTER] megnyomására állapotot vált, megjelenik a prompt
+   0: leállítja a futást
+   1,2: átállítja a feliratot
+*/
+
+
 int main()
 {
+    std::string motd = "Message of the day";
+        // thread control
+        std::condition_variable cvar;
+        std::mutex mtx;
 
+        // program stop flag
+        bool over = false;
+        // képfrissítő stop flag
+        bool stopClock = false;
+
+        // képfrissítő függvény mehet ide
+        auto clock_function = [&]() {
+            // egyből meg is állítja magát, amíg nem kap értesítést
+            std::unique_lock<std::mutex> lk(mtx);
+            cvar.wait(lk,[&]() {return !stopClock;});
+            mtx.unlock();
+
+            while (!stopClock)
+            {
+                system("cls");
+                std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
+                printTime(now);
+                mtx.lock();
+                std::cout << motd << std::endl;
+                mtx.unlock();
+                std::this_thread::sleep_for(std::chrono::seconds(60));
+            }
+        };
+
+        std::thread* clock = new std::thread(clock_function);
+
+        while (!over)
+        {
+            std::cin.get();
+            mtx.lock();
+            stopClock = true;
+            mtx.unlock();
+
+            // töröljük a komplet thread-et
+            //clock->join();
+            clock->detach();
+            delete clock;
+            // opciók
+            std::cout << ">" << std::flush;
+            int param = 0;
+            std::cin >> param;
+            if (param == 0)
+            {
+                std::cout << "game over" << std::endl;
+                over = true;
+            }
+            else
+            {
+                // frissítés létrehozása
+                clock = new std::thread(clock_function);
+
+                //
+                mtx.lock();
+                // megfelelő funkció elindítása
+                switch (param)
+                {
+                case 1: {std::cout << "1-es opcio" << std::endl; motd = "option 1"; break;}
+                case 2: {std::cout << "2-es opcio" << std::endl; motd = "option 2"; break;}
+                }
+
+                // frissítés futási feltétele
+
+                stopClock = false;
+                mtx.unlock();
+
+                // frissítés beindítása
+                cvar.notify_all();
+                std::cin.ignore();
+            }
+
+        }
     return 0;
 }
-/*
-// teljes adatbevitel fileolvasás része
-cout << "Hello World!" << endl;
-    Repuloter& r = Repuloter::getInstance();
-    ifstream input;
-    input.open("tester.csv",ios_base::in);
-    if (!input.is_open())
-        return -1;
-    string line;
-    while (getline(input,line))
-    {
-        // azonosito 0; honnan 1; hova 2; indulas 3; erkezes 4; keses 5
-        vector<string> lineComponents = String::split<vector<string>>(line,';');
-        vector<string> dateComponents = String::split<vector<string>>(lineComponents[3],'-');
-        Datum indul(String::strToNum<unsigned>(dateComponents[0]),String::strToNum<unsigned>(dateComponents[1]),String::strToNum<unsigned>(dateComponents[2]),
-                String::strToNum<unsigned>(dateComponents[3]),String::strToNum<unsigned>(dateComponents[4]));
-        dateComponents.clear();
-        dateComponents = String::split<vector<string>>(lineComponents[4],'-');
-        Datum erkezik(String::strToNum<unsigned>(dateComponents[0]),String::strToNum<unsigned>(dateComponents[1]),String::strToNum<unsigned>(dateComponents[2]),
-                String::strToNum<unsigned>(dateComponents[3]),String::strToNum<unsigned>(dateComponents[4]));
-        Jarat toAdd(lineComponents[0],lineComponents[1],lineComponents[2],indul,erkezik,String::strToNum<unsigned>(lineComponents[5]));
-        r.addJarat(toAdd);
-    }
-*/
